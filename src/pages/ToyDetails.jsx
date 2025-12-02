e897bc41da678e1f1af7294f06036e8976c34be8
User Guy <decoding.c.t@gmail.com>
2025-12-02 20:13:32 +0200

    adding edite and details

---
+++ src/pages/ToyDetails.jsx
@@ -0,0 +1,12 @@
+
+
+export function ToyDetails() {
+
+
+    return (
+        <div className="toy-details">
+            <h1>Toy Details</h1>
+            <p>Here are the details of the selected toy.</p>
+        </div>
+    )
+}
---
+++ src/pages/ToyEdite.jsx
@@ -0,0 +1,11 @@
+
+
+export function ToyEdite() {
+
+    return (
+        <div className="toy-edite">
+            <h1>Toy Edit Page</h1>
+            <p>Edit your toy details here.</p>
+        </div>
+    )
+}
